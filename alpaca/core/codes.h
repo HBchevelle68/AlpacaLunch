66011782408f96dd800dc9642a6d3b1aba09012b
ap <ap@debian-10.1>
2020-01-25 13:50:34 -0500

    adeed comments explaining codes better

--- alpaca/core/codes.h
+++ alpaca/core/codes.h
@@ -5,13 +5,19 @@
 
 typedef uint32_t ALPACA_STATUS;
 
+/*
+ * Function Return Error codes
+ * Maintain posix compliance, this also ensures portability
+ */
+#define ALPACA_SUCCESS  (0)
+#define ALPACA_FAILURE  (1)
 
-#define ALPACA_SUCCESS  0
-#define ALPACA_FAILURE  1
-
-#define ALPACA_TLSINIT  2
-#define ALPACA_TLSCERT  3
-#define ALPACA_TLSKEY   4
+/*
+ * Tool Error Codes 
+ */
+#define ALPACA_TLSINIT  (2)
+#define ALPACA_TLSCERT  (3)
+#define ALPACA_TLSKEY   (4)
 
 
 #endif
