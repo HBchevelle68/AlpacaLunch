9aeeaf037009296f8173cd10c8bae28f1b70982c
ap <ap@debian10>
2020-02-08 22:30:43 -0500

    added mem zero'ing funciton

--- alpaca/interfaces/memory_interface.h
+++ alpaca/interfaces/memory_interface.h
@@ -4,5 +4,9 @@
  */
 #ifndef MEMORY_INTERFACE_H
 #define MEMORY_INTERFACE_H
+#include <stddef.h>
+#include <stdint.h>
+
+extern void* AlpacaMemory_zero(void* buf, size_t bufsize, size_t len);
 
 #endif
--- alpaca/memory/mem.c
+++ alpaca/memory/mem.c
@@ -1,3 +1,15 @@
 #include <interfaces/memory_interface.h>
 
 // TODO
+
+void* AlpacaMemory_zero(void* buf, size_t bufsize, size_t len){
+	
+	if(len > bufsize){
+		len = bufsize;
+	}
+	volatile uint8_t * ptr = buf;
+	while(len--){
+		*ptr++ = (uint8_t)0;
+	}
+	return buf;
+}
