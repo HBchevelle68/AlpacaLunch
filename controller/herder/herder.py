0ece8a17cba7fc92aaf0357baa19fac9cd0da1fe
Chris <chris0parsons@gmail.com>
2020-11-11 17:27:54 -0800

    Fixed argument parsing for herder_bootstrapper.py cleaned up herder code and sucessfully pass arguments from bootstrap to herder

--- controller/herder/herder.py
+++ controller/herder/herder.py
@@ -1,22 +1,27 @@
 
 import cmd2
+import getpass
 import argparse
-
+from controller.herder.herder_auth import HerderAuth
 
 class Herder(cmd2.Cmd):
     """Main Client to connect to a hutch to interact with Alpacas Class"""
 
-    connect_parser = argparse.ArgumentParser()
-    connect_parser.add_argument('-u', '--username',
-                                help='Username to connect to an Alpaca Hutch')
-    # connect_parser.add_argument('-p', '--password',
-    #                            help='Password associated with the selected username')
-    connect_parser.add_argument('-s', '--server', action='append',
-                                help='Servers to connect to. Can be IPs or Domain Names')
-    connect_parser.add_argument('-p', '--port', default=443,
-                                help='Port to connect to. Defaults to 443.')
+    def __init__(self, params):
 
-    @cmd2.with_argparser(connect_parser)
+        print('Welcome to Herder!')
+        print(params.username)
+        print(params.port)
+        print(params.server)
+
+
+
+        # username = input("Username: ")
+        # password = getpass.getpass()
+
+
+
+    #@cmd2.with_argparser(connect_parser)
     def do_connect(self, args):
         """Connects to a hutch"""
         print('Testing Connection...')
--- controller/herder_bootstrapper.py
+++ controller/herder_bootstrapper.py
@@ -1,10 +1,19 @@
-from controller.herder.herder_auth import HerderAuth
+from controller.herder.herder import Herder
 import sys
+from cmd2 import Cmd2ArgumentParser
 
 if __name__ == "__main__":
 
     print('Welcome to Herder')
-    login = HerderAuth()
 
-    #c = Client()
-    #sys.exit(c.cmdloop())
+    cli_parser = Cmd2ArgumentParser()
+    cli_parser.add_argument('-u', '--username',
+                            help='Username to connect to an Alpaca Hutch')
+    cli_parser.add_argument('-s', '--server', default='localhost',
+                            help='Servers to connect to. Can be IPs or Domain Names')
+    cli_parser.add_argument('-p', '--port', default=443,
+                            help='Port to connect to. Defaults to 443.')
+
+    herder = Herder(cli_parser.parse_args())
+
+    sys.exit(Herder.cmdloop())
