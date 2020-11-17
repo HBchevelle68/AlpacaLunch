45527cee37895024b58ddf33521cb07fed75d42c
Chris <chris0parsons@gmail.com>
2020-11-16 16:29:59 -0800

    Added command line arguments to herder_bootstrapper.py Created template client that attempts to connect to a hutch Created command to connect to a hutch and catch socket errors

--- controller/herder/herder.py
+++ controller/herder/herder.py
@@ -1,27 +1,61 @@
-
+import socket
 import cmd2
+import wolfssl
+import wolfcrypt
 import getpass
+import sys
 import argparse
 from controller.herder.herder_auth import HerderAuth
 
+
 class Herder(cmd2.Cmd):
-    """Main Client to connect to a hutch to interact with Alpacas Class"""
+    """Main Client to connect to a hutch to interact with Alpacas Class
+    """
 
-    def __init__(self, params):
+    def __init__(self, username, server='localhost', port=6969):
+        """A command line tool to control your ever growing herd of Alpacas through various hutches.
 
+        :param username: username for authentication to a hutch
+        :param server: ip or hostname of the hutch to connect to, if not specified, defaults to localhost
+        :param port: port to use when connecting ot the hutch
+        """
         print('Welcome to Herder!')
-        print(params.username)
-        print(params.port)
-        print(params.server)
+        _password = getpass.getpass('Please enter your hutch password: ')
 
+        self.hutch_connect(server, port, username, _password)
 
+        # Initialize cmd2
+        super().__init__(allow_cli_args=False)
 
-        # username = input("Username: ")
-        # password = getpass.getpass()
+    def hutch_connect(self, server, port, username, password, timeout=5):
+        """A command line tool to control your ever growing herd of Alpacas through various hutches.
 
+        :param server: ip or hostname of the hutch to connect to
+        :param port: port to use when connecting ot the hutch
+        :param username: username for authentication to a hutch
+        :param password: plaintext password for authentication to a hutch
+        :param timeout: socket timeout for connection attempt, if not specified, defaults to 5 seconds
+        """
 
+        hutch_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 
-    #@cmd2.with_argparser(connect_parser)
+        context = wolfssl.SSLContext(wolfssl.PROTOCOL_TLSv1_2)
+
+        hutch_secure_socket = context.wrap_socket(hutch_socket)
+
+        hutch_secure_socket.settimeout(timeout)
+
+        try:
+            hutch_secure_socket.connect((server, port))
+
+            hutch_secure_socket.write("Testing 1 2 3...")
+
+            print(hutch_secure_socket.read())
+
+        except socket.error as error:
+            print(error)
+
+    # @cmd2.with_argparser(connect_parser)
     def do_connect(self, args):
         """Connects to a hutch"""
         print('Testing Connection...')
--- controller/herder_bootstrapper.py
+++ controller/herder_bootstrapper.py
@@ -4,16 +4,14 @@ from cmd2 import Cmd2ArgumentParser
 
 if __name__ == "__main__":
 
-    print('Welcome to Herder')
-
     cli_parser = Cmd2ArgumentParser()
     cli_parser.add_argument('-u', '--username',
                             help='Username to connect to an Alpaca Hutch')
     cli_parser.add_argument('-s', '--server', default='localhost',
                             help='Servers to connect to. Can be IPs or Domain Names')
-    cli_parser.add_argument('-p', '--port', default=443,
+    cli_parser.add_argument('-p', '--port', default=443, type=int,
                             help='Port to connect to. Defaults to 443.')
 
     herder = Herder(cli_parser.parse_args())
 
-    sys.exit(Herder.cmdloop())
+    sys.exit(herder.cmdloop())
